499d42cc63e906dc1c800fac82e28a875b6e0f86
gadea28 <131365266+gadea28@users.noreply.github.com>
2024-01-02 18:24:45 +0100

    Add files via upload

---
+++ Gadea-crud/web/js/funciones.js
@@ -0,0 +1,14 @@
+/**
+ * Funciones auxiliares de javascripts 
+ */
+function confirmarBorrar(nombre,id){
+  if (confirm("¿Quieres eliminar el usuario:  "+nombre+"?"))
+  {
+   document.location.href="?orden=Borrar&id="+id;
+  }
+}
+
+
+function mostrarclave() {
+   alert(" Método no implementado en Javascript ")
+} 
